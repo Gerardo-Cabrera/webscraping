1b8f054dacf669d379a07220f037c3081cba38bf
Gerardo-Cabrera <gerardocabrera18@gmail.com>
2023-04-16 12:44:02 -0700

    Modification of variables and comments in English and two functions were added to save the information in txt and json files

--- webscraping.py
+++ webscraping.py
@@ -1,42 +1,56 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
 
-# Función para realizar web scraping en la página de "Top 250" de IMDb
-def obtener_top_peliculas(num_peliculas=10):
+# Function to perform web scraping on the IMDb "Top 250" page
+def get_top_movies(movies_num=10):
     url = "https://www.imdb.com/chart/top/"
 
-    # Realizar una solicitud HTTP GET para obtener el contenido de la página web
-    respuesta = requests.get(url)
-    contenido_web = respuesta.content
+    # Make an HTTP GET request to get the content of the web page
+    response = requests.get(url)
+    web_content = response.content
 
-    # Utilizar BeautifulSoup para analizar el contenido HTML
-    soup = BeautifulSoup(contenido_web, "html.parser")
+    # Use BeautifulSoup to parse HTML content
+    soup = BeautifulSoup(web_content, "html.parser")
 
-    # Buscar todos los elementos de la tabla de películas
-    tabla_peliculas = soup.find("tbody", class_="lister-list")
-    filas_peliculas = tabla_peliculas.find_all("tr")[:num_peliculas]
+    # Find all the elements of the cinema table
+    movies_table = soup.find("tbody", class_="lister-list")
+    movies_rows = movies_table.find_all("tr")[:movies_num]
 
-    # Extraer la información de cada película y agregarla a una lista
-    lista_peliculas = []
-    for fila in filas_peliculas:
-        titulo = fila.find("td", class_="titleColumn").a.text
-        año = fila.find("span", class_="secondaryInfo").text.strip("()")
-        rating = fila.find("td", class_="ratingColumn imdbRating").strong.text
+    # Extract information from each movie and add it to a list
+    movies_list = []
+    for row in movies_rows:
+        title = row.find("td", class_="titleColumn").a.text
+        year = row.find("span", class_="secondaryInfo").text.strip("()")
+        rating = row.find("td", class_="ratingColumn imdbRating").strong.text
 
-        pelicula = {
-            "titulo": titulo,
-            "año": año,
+        movie = {
+            "title": title,
+            "year": year,
             "rating": rating
         }
-        lista_peliculas.append(pelicula)
+        movies_list.append(movie)
 
-    # Imprimir la lista de películas
-    for index, pelicula in enumerate(lista_peliculas, start=1):
-        print(f"{index}. {pelicula['titulo']} ({pelicula['año']}) - Rating: {pelicula['rating']}")
+    # Print the list of movies
+    for index, movie in enumerate(movies_list, start=1):
+        print(f"{index}. {movie['title']} ({movie['year']}) - Rating: {movie['rating']}")
+
+    # Return the list of movies
+    return movies_list
+
+def save_txt(movies):
+    with open("movies.txt", "w", encoding="utf-8") as txt_file:
+        for index, movie in enumerate(movies, start=1):
+            txt_file.write(f"{index}. {movie['title']} ({movie['year']}) - Rating: {movie['rating']}\n")
+
+def save_json(movies):
+    with open("movies.json", "w", encoding="utf-8") as json_file:
+        json.dump(movies, json_file, ensure_ascii=False, indent=4)
 
 
-# Llamar a la función para mostrar las películas
+# Call the functions to display the list of movies and save this information in txt and json files
 if __name__ == "__main__":
-    obtener_top_peliculas()
-    
+    top_movies = get_top_movies()
+    save_txt(top_movies)
+    save_json(top_movies)
